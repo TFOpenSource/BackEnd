451dd2b06daa7192351d2d89000b00761475144f
Jesus <jesus.cont5648@gmail.com>
2024-11-02 16:50:06 -0500

    the endpoint of Subscriptions has been added.

--- src/main/java/com/acme/nutrimove/platform/fitness/domain/model/commands/DeleteActivityCommand.java
+++
@@ -1,7 +0,0 @@
-package com.acme.nutrimove.platform.fitness.domain.model.commands;
-
-public record DeleteActivityCommand(Long activityId) {
-    public DeleteActivityCommand {
-        if (activityId == null) throw new IllegalArgumentException("activityId is null");
-    }
-}
--- src/main/java/com/acme/nutrimove/platform/fitness/domain/model/queries/GetAllActivityByNameQuery.java
+++
@@ -1,7 +0,0 @@
-package com.acme.nutrimove.platform.fitness.domain.model.queries;
-
-public record GetAllActivityByNameQuery(String name) {
-    public GetAllActivityByNameQuery {
-        if (name == null) throw new NullPointerException("name is null");
-    }
-}
