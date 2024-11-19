3101adcbbe6c94de1f874d94e6dc8cdc5fb2dcea
Sebasbl562 <gerardo.sebastian.bl@gmail.com>
2024-11-19 13:32:10 -0500

    hotfix Subscription relations with user

--- src/main/java/com/acme/nutrimove/platform/backend/subscriptions/interfaces/rest/transform/CreateSubscriptionCommandFromResourceAssembler.java
+++ src/main/java/com/acme/nutrimove/platform/backend/subscriptions/interfaces/rest/transform/CreateSubscriptionCommandFromResourceAssembler.java
@@ -2,9 +2,11 @@ package com.acme.nutrimove.platform.backend.subscriptions.interfaces.rest.transf
 
 import com.acme.nutrimove.platform.backend.subscriptions.domain.model.commands.CreateSubscriptionCommand;
 import com.acme.nutrimove.platform.backend.subscriptions.interfaces.rest.resources.CreateSubscriptionResource;
+import com.acme.nutrimove.platform.backend.user.domain.model.aggregates.User;
 
 public class CreateSubscriptionCommandFromResourceAssembler {
-    public static CreateSubscriptionCommand toCommand(CreateSubscriptionResource resource) {
-        return new CreateSubscriptionCommand(resource.description(), resource.price(), resource.monthDuration(), resource.trial(), resource.userId());
+    public static CreateSubscriptionCommand toCommand(CreateSubscriptionResource resource, User user) {
+        return new CreateSubscriptionCommand(resource.description(), resource.price(), resource.monthDuration(),
+                resource.trial(), user);
     }
 }
--- src/main/java/com/acme/nutrimove/platform/backend/user/domain/model/aggregates/User.java
+++ src/main/java/com/acme/nutrimove/platform/backend/user/domain/model/aggregates/User.java
@@ -1,6 +1,7 @@
 package com.acme.nutrimove.platform.backend.user.domain.model.aggregates;
 
 import com.acme.nutrimove.platform.backend.Hydration.domain.model.aggregates.Hydration;
+import com.acme.nutrimove.platform.backend.subscriptions.domain.model.aggregates.Subscription;
 import com.acme.nutrimove.platform.backend.user.domain.ValueObjects.Privacy;
 import com.acme.nutrimove.platform.backend.user.domain.model.commands.CreateUserCommand;
 import jakarta.persistence.*;
@@ -49,10 +50,11 @@ public class User {
     @Column(nullable = false)
     private Privacy privacy;
 
-    @OneToMany(mappedBy = "userId", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private List<Hydration> hydrations;
-  
-  
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Subscription subscription;
+
+
+
     public User(CreateUserCommand command) {
         this.name = command.name();
         this.lastname = command.lastname();
