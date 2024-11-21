70174c492dc5e086d921f6352f6236f8b0750319
Jesus <jesus.cont5648@gmail.com>
2024-11-20 23:38:41 -0500

    deploy

--- src/main/java/com/acme/nutrimove/platform/WebConfig.java
+++ src/main/java/com/acme/nutrimove/platform/WebConfig.java
@@ -9,11 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+        registry.addMapping("/api/v1/**")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
-
     }
 }
